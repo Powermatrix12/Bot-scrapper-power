dc5a50193235303bbd7bf78efb096bb290f376c5
Powermatrix12 <167418825+Powermatrix12@users.noreply.github.com>
2024-10-26 15:56:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-API_ID = os.getenv("API_ID")
-API_HASH = os.getenv("API_HASH")
+API_ID = os.getenv("26258063")
+API_HASH = os.getenv("be0a0e2ecd938bfc5401d35a399deeb7")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-MONGO_URI = os.getenv("MONGO_URI")
-OWNER_ID = int(os.getenv("OWNER_ID"))
+MONGO_URI = os.getenv("mongodb+srv://zuuvou:XNWdDOsGFprx7Cnw@clusterop.pnyvj.mongodb.net/?retryWrites=true&w=majority")
+OWNER_ID = int(os.getenv("5904478052"))
